f22497ca18e0e193c2b48f164b0268037e5a3876
Ben Grunfeld <benjamin.grunfeld@gmail.com>
2013-10-28 11:53:41 -0600

    Added environment variables

--- src/main/scala/web.scala
+++ src/main/scala/web.scala
@@ -23,7 +23,7 @@ class Hello extends Service[HttpRequest, HttpResponse] {
   def apply(req: HttpRequest): Future[HttpResponse] = {
     val response = Response()
     response.setStatusCode(200)
-    response.setContentString("Powered by Deis.")
+    response.setContentString("Powered by" + System.getenv("POWERED_BY"))
     Future(response)
   }
 }
